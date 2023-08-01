349c99b4df5a17b494364a240a1d2c56c1ae624d
Lakshita <98271965+lakshita157@users.noreply.github.com>
2023-08-01 22:53:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ searchBtn.addEventListener("click", () => {
     fetch(finalURL)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data[0]);
-            console.log(data[0].capital[0]);
+            // console.log(data[0]);
+            // console.log(data[0].capital[0]);
             console.log(data[0].flags.svg);
             console.log(data[0].name.common);
             console.log(data[0].continents[0]);
@@ -64,5 +64,7 @@ searchBtn.addEventListener("click", () => {
             }
         });
 });
+
+
 
 
